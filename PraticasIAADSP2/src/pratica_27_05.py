e24810f72b9abd77e268248904cc093152116899
mr.sh1ft <sh1ft.developer144k@gmaol.com>
2025-05-27 21:17:31 -0300

    Up v1.0

--- PraticasIAADSP2/src/pratica_27_05.py
+++ PraticasIAADSP2/src/pratica_27_05.py
@@ -8,6 +8,7 @@ print(pd.__version__)
 # Lendo o arquivo CSV
 print("Exibindo um arquivo .CSV")
 df = pd.read_csv('database/iris.csv')
+df.columns = ['sepal_lenght', 'sepal_width','petal_lenght',  'petal_width', 'class']
 
 # Mostrando formato dos dados
 print("Exibindo o formato de dado:")
@@ -15,3 +16,9 @@ print('Mostrando o shape: ')
 print(df.shape)
 print('------------------------------------------------------------')
 
+print(df.columns)
+df['class'] = df['class'].astype('category')
+df['class1'] = df['class'].cat.codes
+print(df.info())
+X = df.loc[: , 'sepal_lenght': 'petal_width']
+y = df['class1']
